a1cec40ce6338baf53492100ae469a65ece3fc5e
Anupam Biswas <anupambiswas997@gmail.com>
2025-07-21 00:19:28 +0000

    added testing methods for linear algebra - ongoing

--- tests/prepare_tests_data.py
+++ tests/prepare_tests_data.py
@@ -15,16 +15,76 @@ def size_t_elem(x):
 def double_elem(x):
     return ['d', x]
 
+# vector -> [1.23, 4.56, -3.45] (can also be in numpy 1d array form)
 def write_vector_data(vector_data, filepath):
     data = [size_t_elem(len(vector_data))] + [double_elem(e) for e in vector_data]
     write_binary_data(data, filepath)
 
-# sparse_vector_data = {'size': 5, 'defval': 0.32, 'content': {1: 3.45, 4: -2.38}}
+# sparse_vector -> (10, # vector size
+#                   3.2, # default value
+#                   {1: -2.34, 4: 0.0032, 8: -8.54}) # content
 def write_sparse_vector_data(sparse_vector_data, filepath):
-    size = sparse_vector_data['size']
-    defval = sparse_vector_data['defval']
-    content = sparse_vector['content']
-    data = [size_t_elem(size), double_elem(defval)]
-    for e in content:
-        data.extend([size_t_elem(e[0]), double_elem(e[1])])
+    size, defval, content = sparse_vector_data
+    data = [size_t_elem(size), double_elem(defval), size_t_elem(len(content.keys()))]
+    for k,v in content.items():
+        data.extend([size_t_elem(k), double_elem(v)])
     write_binary_data(data, filepath)
+
+# matrix -> [[-3, 4.52], [-7.09, 12.32]] (can also be in numpy 2d array form)
+def write_matrix_data(matrix_data, filepath):
+    nrows, ncols = matrix_data.shape
+    data = [size_t_elem(nrows), size_t_elem(ncols)]
+    data.extend([double_elem(matrix_data[i][j]) for i in range(nrows) for j in range(ncols)])
+    write_binary_data(data, filepath)
+
+# sparse_matrix -> (2, # number of rows
+#                   3, # number of columns
+#                   8.43, # default value
+#                   {0: {2: 3.01}, 1: {0: 1.2, 2: -0.9}}) # content
+def write_sparse_matrix_data(sparse_matrix_data, filepath):
+    nrows, ncols, defval, content = sparse_matrix_data
+    data = [size_t_elem(nrows), size_t_elem(ncols), double_elem(defval)]
+    content_part = []
+    num_elems = 0
+    for i,v in content.items():
+        for j,value in v.items():
+            content_part.extend([size_t_elem(i), size_t_elem(j), double_elem(value)])
+            num_elems += 1
+    data.append(size_t_elem(num_elems))
+    data.extend(content_part)
+    write_binary_data(data, filepath)
+
+# dim -> (5,) for vector, (3, 4) for matrix
+def get_random_matvec(start, end, dim):
+    diff = end - start
+    return np.random.random(dim) * diff + start
+
+def get_random_sparse_vec(start, end, size, num_elems):
+    diff = end - start
+    defval = np.random.random() * diff + start
+    content = {}
+    added = set()
+    while len(added) < num_elems:
+        i = random.randrange(size)
+        content[i] = np.random.random() * diff + start
+        added.add(i)
+    return (size, defval, content)
+
+def get_random_sparse_mat(start, end, nrows, ncols, num_elems):
+    diff = end - start
+    defval = np.random.random() * diff + start
+    content = {}
+    added = set()
+    while len(added) < num_elems:
+        i = random.randrange(nrows)
+        j = random.randrange(ncols)
+        ij = f"{i}_{j}"
+        if not i in content.keys():
+            content[i] = {}
+        content[i][j] = np.random.random() * diff + start
+        added.add(ij)
+    return (nrows, ncols, defval, content)
+
+if __name__ == '__main__':
+    print(get_random_sparse_mat(-2, 2, 3, 4, 5))
+    print("\n".join([f"i={i},j={j}" for i in range(3) for j in range(6,9)]))
--- tests/test_base.hpp
+++ tests/test_base.hpp
@@ -2,6 +2,11 @@
 #include "sparse_matrix.hpp"
 #include "vectr.hpp"
 #include "sparse_vector.hpp"
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
 
 struct TestParams
 {
@@ -55,4 +60,85 @@ bool areEqual(const VectorTypeA& va, const VectorTypeB& vb, size_t size, double 
 {
     double maxDiff = getMaximumAbsDiff(va, vb, size);
     return (maxDiff <= tolerance);
+}
+
+Vector getVectorFromFile(std::string filepath)
+{
+    std::vector<double> r = {};
+    std::ifstream f;
+    f.open(filepath.c_str(), std::ios::in | std::ios::binary);
+    size_t size;
+    double value;
+    f.read((char*)&size, sizeof(size_t));
+    for(size_t i = 0; i < size; i++)
+    {
+        f.read((char*)&value, sizeof(double));
+        r.push_back(value);
+    }
+    f.close();
+    return Vector(r);
+}
+
+Matrix getMatrixFromFile(std::string filepath)
+{
+    std::vector<std::vector<double> > r = {};
+    std::ifstream f;
+    f.open(filepath.c_str(), std::ios::in | std::ios::binary);
+    size_t numRows, numColumns;
+    double value;
+    f.read((char*)&numRows, sizeof(size_t));
+    f.read((char*)&numColumns, sizeof(size_t));
+    for(size_t i = 0; i < numRows; i++)
+    {
+        r.push_back({});
+        for(size_t j = 0; j < numColumns; j++)
+        {
+            f.read((char*)&value, sizeof(double));
+            r[i].push_back(value);
+        }
+    }
+    f.close();
+    return Matrix(r);
+}
+
+SparseVector getSparseVectorFromFile(std::string filepath)
+{
+    double value;
+    size_t size, numElems, i;
+    std::ifstream f;
+    f.open(filepath.c_str(), std::ios::in | std::ios::binary);
+    f.read((char*)&size, sizeof(size_t));
+    f.read((char*)&value, sizeof(double));
+    f.read((char*)&numElems, sizeof(size_t));
+    SparseVector sv(value, size);
+    for(size_t k = 0 ; k < numElems; k++)
+    {
+        f.read((char*)&i, sizeof(size_t));
+        f.read((char*)&value, sizeof(double));
+        sv[i] = value;
+    }
+    f.close();
+    return sv;
+}
+
+SparseMatrix getSparseMatrixFromFile(std::string filepath)
+{
+    size_t numRows, numColumns, i, j, numElems;
+    double value;
+    std::ifstream f;
+    f.open(filepath.c_str(), std::ios::in | std::ios::binary);
+    f.read((char*)&numRows, sizeof(size_t));
+    f.read((char*)&numColumns, sizeof(size_t));
+    f.read((char*)&value, sizeof(double));
+    SparseMatrix sm(value, numRows, numColumns);
+    f.read((char*)&numElems, sizeof(size_t));
+    for(size_t k = 0; k < numElems; k++)
+    {
+        f.read((char*)&i, sizeof(size_t));
+        f.read((char*)&j, sizeof(size_t));
+        f.read((char*)&value, sizeof(double));
+        sm[i][j] = value;
+    }
+    f.close();
+    return sm;
 }
