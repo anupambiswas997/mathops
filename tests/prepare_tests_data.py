377781b934cbb641b12a6fd4c3a6670713b1db3a
Anupam Biswas <anupambiswas997@gmail.com>
2025-07-22 04:45:15 +0000

    adding tests prep code - ongoing

--- tests/prepare_tests_data.py
+++ tests/prepare_tests_data.py
@@ -57,24 +57,29 @@ def write_sparse_matrix_data(sparse_matrix_data, filepath):
 # dim -> (5,) for vector, (3, 4) for matrix
 def get_random_matvec(start, end, dim):
     diff = end - start
-    return np.random.random(dim) * diff + start
+    matvec = np.random.random(dim) * diff + start
+    return matvec, matvec
 
 def get_random_sparse_vec(start, end, size, num_elems):
     diff = end - start
     defval = np.random.random() * diff + start
     content = {}
     added = set()
+    spvec_np = np.full((size,), defval)
     while len(added) < num_elems:
         i = random.randrange(size)
         content[i] = np.random.random() * diff + start
+        spvec_np[i] = content[i]
         added.add(i)
-    return (size, defval, content)
+    spvec = (size, defval, content,)
+    return spvec, spvec_np
 
 def get_random_sparse_mat(start, end, nrows, ncols, num_elems):
     diff = end - start
     defval = np.random.random() * diff + start
     content = {}
     added = set()
+    spmat_np = np.full((nrows, ncols), defval)
     while len(added) < num_elems:
         i = random.randrange(nrows)
         j = random.randrange(ncols)
@@ -82,9 +87,50 @@ def get_random_sparse_mat(start, end, nrows, ncols, num_elems):
         if not i in content.keys():
             content[i] = {}
         content[i][j] = np.random.random() * diff + start
+        spmat_np[i][j] = content[i][j]
         added.add(ij)
-    return (nrows, ncols, defval, content)
+    spmat = (nrows, ncols, defval, content,)
+    return spmat, spmat_np
+
+def indent(text, ind):
+    return "\n".join([f"{ind}{l}" for l in text.split("\n")])
+
+def pad(text, maxlen):
+    tstr = str(text)
+    return tstr + (((maxlen - len(tstr)) * ' ') if maxlen > len(tstr) else '')
+
+def get_matrix_text(m, var_name):
+    maxlen = max([len(str(m[i][j])) for i in range(m.shape[0]) for j in range(m.shape[1])])
+    lines = [('{' + ', '.join([pad(e, maxlen) for e in m[i]]) + '}') for i in range(m.shape[0])]
+    return f"Matrix {var_name}(" + "{\n    " + ",\n    ".join(lines) + "\n});"
+
+def get_vector_text(v, var_name):
+    maxlen = max([len(str(e)) for e in v])
+    return f"Vector {var_name}(" + "{" + ', '.join([pad(e, maxlen) for e in v]) + "});"
+
+def get_sparse_matrix_text(sm, var_name):
+    nrows, ncols, defval, content = sm
+    sm_text = f"SparseMatrix {var_name}({defval}, {nrows}, {ncols});"
+    for i,v in content.items():
+        for j,value in v.items():
+            sm_text += f"\n{var_name}[{i}][{j}] = {value};"
+    return sm_text
+
+def get_sparse_vector_text(sv, var_name):
+    size, defval, content = sv
+    sv_text = f"SparseVector {var_name}({defval}, {size});"
+    for i,value in content.items():
+        sv_text += f"\n{var_name}[{i}] = {value};"
+    return sv_text
+
+# create_test('m', (5, 3), 'sm', (3, 4), '*')
+def create_test(typ1, dim1, typ2, dim2, oper):
+    pass
 
 if __name__ == '__main__':
-    print(get_random_sparse_mat(-2, 2, 3, 4, 5))
-    print("\n".join([f"i={i},j={j}" for i in range(3) for j in range(6,9)]))
+    #print(get_random_sparse_mat(-2, 2, 3, 4, 5))
+    #print("\n".join([f"i={i},j={j}" for i in range(3) for j in range(6,9)]))
+    print(get_matrix_text(get_random_matvec(0, 1, (3, 4))[0], "ma"))
+    print(get_vector_text(get_random_matvec(-2, 2, (5,))[0], "vb"))
+    print(get_sparse_matrix_text(get_random_sparse_mat(-3, 3, 4, 3, 5)[0], "smc"))
+    print(get_sparse_vector_text(get_random_sparse_vec(-10, 10, 20, 7)[0], "svd"))
